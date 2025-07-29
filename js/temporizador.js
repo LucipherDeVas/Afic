7c7d0750aef7fba7afa5eec7d3698adb238c0c1c
LucipherDeVas <cristopher.vasquez1@mail.udp.cl>
2025-07-29 02:34:13 -0400

    temporizador 1.3 (Version funcional)

--- js/temporizador.js
+++ js/temporizador.js
@@ -1,5 +1,4 @@
-
-
+//Esto me permite establecer los tiempos al temporizador y dirige hacia el boton iniciar de manera suave
 function tiempo1() {
     tiempo = document.getElementById("tiempo2").innerHTML = "00:15:00"
     document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
@@ -20,3 +19,59 @@ function tiempo5() {
     tiempo = document.getElementById("tiempo2").innerHTML = "01:00:00"
     document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
 }
+
+//Variable para que no se aprete multiples veces el tiempo
+let iniciado = false
+
+function iniciar() {
+
+    if (iniciado === false) {
+        iniciado = true
+        intervalo = setInterval(() => {
+
+            tiempo = document.getElementById("tiempo2").innerHTML
+
+            let [horas, minutos, segundos] = tiempo.split(':')
+
+            //Transformar de string a entero
+            horas = parseInt(horas, 10)
+            minutos = parseInt(minutos, 10)
+            segundos = parseInt(segundos, 10)
+
+            //Caso borde para que los numeros no sean negativos
+            if(horas>0 || minutos>0 || segundos>0){
+                segundos--
+            if (segundos < 0) {
+                segundos = 59;
+                minutos--;
+
+                if (minutos < 0) {
+                    minutos = 59;
+                    horas--;
+
+                    if (horas < 0) {
+                        iniciado = false
+                        clearInterval(intervalo);
+                    }
+                }
+
+            }
+
+            //Transformar de entero a string
+            horas = horas.toString().padStart(2, '0')
+            minutos = minutos.toString().padStart(2, '0')
+            segundos = segundos.toString().padStart(2, '0')
+
+            document.getElementById("tiempo2").innerHTML = horas + ":" + minutos + ":" + segundos
+
+            }
+
+            //Logica del reloj
+            
+            //El 1000 es porque es 1 seg
+        }, 1000)
+    }
+
+
+}
+
