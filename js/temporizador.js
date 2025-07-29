634758acd2282a519a82afc53ad99bbda71ac5b2
LucipherDeVas <cristopher.vasquez1@mail.udp.cl>
2025-07-28 22:48:12 -0400

    zona de enfoque 1.2

---
+++ js/temporizador.js
@@ -0,0 +1,22 @@
+
+
+function tiempo1() {
+    tiempo = document.getElementById("tiempo2").innerHTML = "00:15:00"
+    document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
+}
+function tiempo2() {
+    tiempo = document.getElementById("tiempo2").innerHTML = "00:25:00"
+    document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
+}
+function tiempo3() {
+    tiempo = document.getElementById("tiempo2").innerHTML = "00:30:00"
+    document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
+}
+function tiempo4() {
+    tiempo = document.getElementById("tiempo2").innerHTML = "00:45:00"
+    document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
+}
+function tiempo5() {
+    tiempo = document.getElementById("tiempo2").innerHTML = "01:00:00"
+    document.getElementById("iniciar").scrollIntoView({ behavior: "smooth" })
+}
